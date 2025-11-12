7166bfb557fd2d8c175bcae2b8e22bff29752945
Vasja Pavlov <vasja.pavlov@truecaller.com>
2025-11-12 10:10:25 +0100

    Initial Commit

---
+++ Labdays_Nov_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Labdays_Nov_2025
+//
+//  Created by Vasja Pavlov on 2025-11-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Labdays_Nov_2025/Labdays_Nov_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Labdays_Nov_2025App.swift
+//  Labdays_Nov_2025
+//
+//  Created by Vasja Pavlov on 2025-11-12.
+//
+
+import SwiftUI
+
+@main
+struct Labdays_Nov_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
